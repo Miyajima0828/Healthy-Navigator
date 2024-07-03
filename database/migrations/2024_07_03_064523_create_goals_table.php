834757d7e062279876e8d5b6c9aedeba42ee44de
Miyajima0828 <miyajima08281234@gmail.com>
2024-07-03 18:03:37 +0900

    [add]goalsテーブルにおけるmodelとmigrationファイルの追加

---
+++ database/migrations/2024_07_03_064523_create_goals_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('goals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->integer('calories')->unsigned()->comment('カロリー kcal');
+            $table->float('protein')->unsigned()->comment('タンパク質  g');
+            $table->float('fat')->unsigned()->comment('脂質 g');
+            $table->float('carbohydrate')->unsigned()->comment('炭水化物 g');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('goals');
+    }
+};
