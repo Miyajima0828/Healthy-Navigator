198f0c50cbd6c55eb9be346827ac3c17d920cd70
Miyajima0828 <miyajima08281234@gmail.com>
2024-07-11 12:08:14 +0900

    [add]食品関連の処理を行うサービスクラスを追加

---
+++ app/Services/MealService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Food;
+use App\Models\Meal;
+use App\Models\MealFood;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Exceptions\BadRequestException;
+
+/**
+ * 食品関連の処理を行うサービスクラス
+ */
+class MealService
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store($request)
+    {
+        try {
+            $this->validateMealRequest($request);
+            DB::beginTransaction();
+            $meal = Meal::query()->create([
+                'user_id' => Auth::id(),
+                'date' => $request['date'],
+                'meal_type' => $request['meal_type'],
+            ]);
+
+            foreach ($request['foods'] as $food) {
+                MealFood::query()->create([
+                    'meal_id' => $meal->id,
+                    'food_id' => $food['id'],
+                    'quantity' => $food['quantity'],
+                ]);
+            }
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    private function validateMealRequest($request)
+    {
+        if (!$this->areAllFoodsExist($request)) {
+            throw new BadRequestException('リクエストが不正です');
+        }
+    }
+
+    private function areAllFoodsExist($request): bool
+    {
+        foreach ($request['foods'] as $food) {
+            if (!Food::where([
+                'id' => $food['id'],
+                'name' => $food['name'],
+            ])->exists()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
