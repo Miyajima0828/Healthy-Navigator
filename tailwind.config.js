900d0d451c54a7a412bdcdf97659f9b3aa291408
Miyajima0828 <miyajima08281234@gmail.com>
2024-07-11 14:29:21 +0900

    [add]tailwind.configにフラッシュメッセージ用のCSSを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,17 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            keyframes: {
+                flashFade: {
+                    "0%": { transform: "translateX(180px)", opacity: 0 },
+                    "20%": { transform: "translateX(0)", opacity: 1 },
+                    "80%": { transform: "translateX(0)", opacity: 1 },
+                    "100%": { transform: "translateX(180px)", opacity: 0 },
+                },
+            },
+            animation: {
+                flash: "flashFade 7.0s forwards",
+            },
         },
     },
 
