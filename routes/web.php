1d02384eeecff54f05808bba32e075ac49915713
Miyajima0828 <miyajima08281234@gmail.com>
2024-07-11 14:34:07 +0900

    [update]ホーム画面のroutingを修正

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Livewire\HomeComponent;
 use App\Livewire\InsertMealComponent;
 use App\Livewire\InsertMealPage;
 use Illuminate\Support\Facades\Route;
@@ -14,7 +14,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/home', [HomeComponent::class, 'render'])->name('home');
     Route::get('meal/create', InsertMealPage::class)->name('meal.create');
     Route::post('meal/store', [InsertMealComponent::class, 'insertMeal'])->name('meal.store');
 });
