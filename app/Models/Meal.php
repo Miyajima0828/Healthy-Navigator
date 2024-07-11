cc6fe078912ccaff4c8a3bd4ef551c9767510048
Miyajima0828 <miyajima08281234@gmail.com>
2024-07-11 12:07:24 +0900

    [add]各モデルのプロパティを設定

--- app/Models/Meal.php
+++ app/Models/Meal.php
@@ -4,8 +4,46 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Carbon\Carbon;
 
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property date $date
+ * @property int $meal_type
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property User $user
+ * @property Food $foods
+ */
 class Meal extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'date',
+        'meal_type',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function foods()
+    {
+        return $this->belongsToMany(Food::class)->using(MealFood::class);
+    }
 }
