415795231a213618474bcf5a3d83ea1c0310c62a
Miyajima0828 <miyajima08281234@gmail.com>
2024-07-17 10:47:27 +0900

    [update]目標設定のルーティングを追加し、APIにログインと目標設定を追加

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Goal\GoalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('login', [LoginController::class, 'login'])->name('api.login');
+
+Route::middleware('auth:sanctum')
+    ->group(function () {
+
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+        Route::put('/goal/update', [GoalController::class, 'upsert'])->name('goal.update');
+    });
