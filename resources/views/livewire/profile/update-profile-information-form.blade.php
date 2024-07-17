e9022b2dcfb5f6404ba41137537835023910e65a
Miyajima0828 <miyajima08281234@gmail.com>
2024-07-17 16:17:04 +0900

    [update]プロフィールを更新するためのBladeの修正

---
+++ resources/views/livewire/profile/update-profile-information-form.blade.php
@@ -0,0 +1,75 @@
+<x-form-section submit="updateProfileInformation">
+    <x-slot name="title">
+        {{ __('Profile Information') }}
+    </x-slot>
+
+    <x-slot name="description">
+        {{ __('Update your account\'s profile information and email address.') }}
+    </x-slot>
+
+    <x-slot name="form">
+        <!-- Name -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="name" value="{{ __('Name') }}" />
+            <x-input id="name" type="text" class="mt-1 block w-full" wire:model="state.name" required
+                autocomplete="name" />
+            <x-input-error for="name" class="mt-2" />
+        </div>
+
+        <!-- Email -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="email" value="{{ __('Email') }}" />
+            <x-input id="email" type="email" class="mt-1 block w-full" wire:model="state.email" required
+                autocomplete="username" />
+            <x-input-error for="email" class="mt-2" />
+
+            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::emailVerification()) &&
+                    !$this->user->hasVerifiedEmail())
+                <p class="text-sm mt-2 dark:text-white">
+                    {{ __('Your email address is unverified.') }}
+
+                    <button type="button"
+                        class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                        wire:click.prevent="sendEmailVerification">
+                        {{ __('Click here to re-send the verification email.') }}
+                    </button>
+                </p>
+
+                @if ($this->verificationLinkSent)
+                    <p class="mt-2 font-medium text-sm text-green-600 dark:text-green-400">
+                        {{ __('A new verification link has been sent to your email address.') }}
+                    </p>
+                @endif
+            @endif
+        </div>
+
+        <!-- Age -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="age" value="{{'年齢'}}" />
+            <x-input id="age" type="number" class="mt-1 block w-full" wire:model="state.age"
+                autocomplete="age" />
+            <x-input-error for="age" class="mt-2" />
+        </div>
+
+        <!-- Gender -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="gender" value="{{'性別'}}" />
+            <select name="gender" class="border rounded" wire:model="state.gender">
+                <option value="0">男性</option>
+                <option value="1">女性</option>
+                <option value="2">その他</option>
+            </select>
+            <x-input-error for="gender" class="mt-2" />
+        </div>
+    </x-slot>
+
+    <x-slot name="actions">
+        <x-action-message class="me-3" on="saved">
+            {{ __('Saved.') }}
+        </x-action-message>
+
+        <x-button wire:loading.attr="disabled" wire:target="photo">
+            {{ __('Save') }}
+        </x-button>
+    </x-slot>
+</x-form-section>
