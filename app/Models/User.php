d1167549b7e6723a11b7d8766c1dc41b1efd91bd
Miyajima0828 <miyajima08281234@gmail.com>
2024-07-17 10:07:34 +0900

    [update]モデルのPHPDocの書き方を統一

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,27 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @param string name
+ * @param string email
+ * @param string password
+ * @param string remember_token
+ * @param string two_factor_recovery_codes
+ * @param string two_factor_secret
+ * @param datetime email_verified_at
+ * @param datetime created_at
+ * @param datetime updated_at
+ * @param string profile_photo_path
+ * @param int age
+ * @param int gender
+ */
 class User extends Authenticatable
 {
     use HasApiTokens;
@@ -18,48 +33,42 @@ class User extends Authenticatable
     use Notifiable;
     use TwoFactorAuthenticatable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'age',
+        'gender'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
         'two_factor_recovery_codes',
         'two_factor_secret',
+        'two_factor_confirmed_at',
     ];
 
-    /**
-     * The accessors to append to the model's array form.
-     *
-     * @var array<int, string>
-     */
     protected $appends = [
         'profile_photo_url',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
         ];
+    }
+
+    /**
+     * 目標とのリレーション
+     * @return HasOne
+     */
+    public function goal(): HasOne
+    {
+        return $this->hasOne(Goal::class);
     }
 }
