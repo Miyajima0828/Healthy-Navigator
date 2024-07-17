73f46134fb6cabe45824a8e5a1e725a51148d497
Miyajima0828 <miyajima08281234@gmail.com>
2024-07-17 10:30:23 +0900

    [add]目標設定を登録するサービスクラスを追加

---
+++ app/Services/Goal/UpsertGoalService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services\Goal;
+
+use App\Models\Goal;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * 目標設定関連の処理を行うサービスクラス
+ */
+class UpsertGoalService
+{
+    /**
+     * 目標設定が存在すれば更新、存在しない場合は新規作成
+     */
+    public function upsert($goal): void
+    {
+        $user = Auth::user();
+        $result = Goal::query()->updateOrCreate(
+            ['user_id' => $user->id],
+            [
+                'user_id' => $user->id,
+                'calorie' => $goal['calorie'],
+                'protein' => $goal['protein'],
+                'fat' => $goal['fat'],
+                'carbohydrate' => $goal['carbohydrate'],
+            ]
+        );
+    }
+}
