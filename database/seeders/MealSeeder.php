b448d74193f93f58de9abb435eba60d65426f032
Miyajima0828 <miyajima08281234@gmail.com>
2024-07-22 11:47:47 +0900

    [add]食事のテストデータを作成するSeederとFactoryを追加

---
+++ database/seeders/MealSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Food;
+use App\Models\Meal;
+use Illuminate\Database\Seeder;
+
+class MealSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $date = now();
+        $startOfDate = $date->startOfWeek()->subWeek();
+        $mealTypes = [0, 1, 2, 3];
+
+        // ランダムなFoodレコード20件を取得
+        $foods = Food::inRandomOrder()->limit(30)->get();
+
+        foreach (range(0, 20) as $i) {
+            foreach ($mealTypes as $mealType) {
+                $meal = [
+                    'user_id' => 1,
+                    'date' => $startOfDate->copy()->addDays($i),
+                    'meal_type' => $mealType,
+                ];
+                $mealFood = $foods->random(3)->mapWithKeys(function ($food) {
+                    return [
+                        $food->id => ['quantity' => rand(100, 300),]
+
+                    ];
+                })->toArray();
+                $mealId = Meal::factory()->create($meal)->id;
+                Meal::find($mealId)->foods()->attach($mealFood);
+            }
+        }
+    }
+}
