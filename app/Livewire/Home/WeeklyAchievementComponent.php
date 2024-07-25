b4ded9daee691888b8cbb89eaa6e0a42836a7ab2
Miyajima0828 <miyajima08281234@gmail.com>
2024-07-25 16:55:31 +0900

    [update]目標が設定されていないとき場合の記述および変数の型を定義するように修正。また、変数名をキャメルケースで定義するように修正

--- app/Livewire/Home/WeeklyAchievementComponent.php
+++ app/Livewire/Home/WeeklyAchievementComponent.php
@@ -6,52 +6,53 @@ use App\Services\Goal\GetGoalServiceInterface;
 use Carbon\Carbon;
 use App\Services\Meal\GetMealServiceInterface;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection as SupportCollection;
 use Livewire\Component;
 
 class WeeklyAchievementComponent extends Component
 {
     public Collection $meals;
-    public $weeklyGoal;
-    public $weeklyNutrient;
-    public $nutrientAchievement;
-    protected GetMealServiceInterface $GetMealService;
-    protected GetGoalServiceInterface $GetGoalService;
+    public ?SupportCollection $weeklyGoal;
+    public array $weeklyNutrient;
+    public string $nutrientAchievement;
+    protected GetMealServiceInterface $getMealService;
+    protected GetGoalServiceInterface $getGoalService;
 
     public function mount()
     {
-        $this->meals = $this->GetMealService->getMealRecords(Carbon::today()->copy()->startOfWeek(), Carbon::today()->copy()->endOfWeek());
-        $this->weeklyGoal = collect($this->GetGoalService->getGoal())
+        $this->meals = $this->getMealService->getMealRecords(Carbon::today()->copy()->startOfWeek(), Carbon::today()->copy()->endOfWeek());
+        $this->weeklyGoal = $this->getGoalService->getGoal() ? collect($this->getGoalService->getGoal())
             ->only('calorie', 'protein', 'fat', 'carbohydrate')
             ->map(function ($value) {
                 return $value * 7;
-            });
+            }) : null;
         $this->weeklyNutrient = [
-            'calorie' => $this->GetMealService->calculateNutrientSum($this->meals, 'calorie'),
-            'protein' => $this->GetMealService->calculateNutrientSum($this->meals, 'protein'),
-            'fat' => $this->GetMealService->calculateNutrientSum($this->meals, 'fat'),
-            'carbohydrate' => $this->GetMealService->calculateNutrientSum($this->meals, 'carbohydrate')
+            'calorie' => $this->getMealService->calculateNutrientSum($this->meals, 'calorie'),
+            'protein' => $this->getMealService->calculateNutrientSum($this->meals, 'protein'),
+            'fat' => $this->getMealService->calculateNutrientSum($this->meals, 'fat'),
+            'carbohydrate' => $this->getMealService->calculateNutrientSum($this->meals, 'carbohydrate')
         ];
         $this->nutrientAchievement = $this->generateNutrientAchievementJson($this->weeklyNutrient);
     }
 
     /**
      * サービスクラスをDIするメソッド
-     * @param GetMealServiceInterface $GetMealService
+     * @param GetMealServiceInterface $getMealService
      */
-    public function boot(GetMealServiceInterface $GetMealService, GetGoalServiceInterface $GetGoalService)
+    public function boot(GetMealServiceInterface $getMealService, GetGoalServiceInterface $getGoalService)
     {
-        $this->GetMealService = $GetMealService;
-        $this->GetGoalService = $GetGoalService;
+        $this->getMealService = $getMealService;
+        $this->getGoalService = $getGoalService;
     }
 
     public function generateNutrientAchievementJson($weeklyNutrient)
     {
-        return json_encode([
-            round($weeklyNutrient['calorie']/ $this->weeklyGoal['calorie'] * 100),
-            round($weeklyNutrient['protein'] / $this->weeklyGoal['protein'] * 100),
-            round($weeklyNutrient['fat'] / $this->weeklyGoal['fat'] * 100),
-            round($weeklyNutrient['carbohydrate'] / $this->weeklyGoal['carbohydrate'] * 100)
-        ]);
+        return $this->weeklyGoal ? json_encode([
+            $this->weeklyGoal['calorie'] !== 0 ? round($weeklyNutrient['calorie']/ $this->weeklyGoal['calorie'] * 100) :0,
+            $this->weeklyGoal['protein'] !== 0 ? round($weeklyNutrient['protein'] / $this->weeklyGoal['protein'] * 100) : 0,
+            $this->weeklyGoal['fat'] !== 0 ? round($weeklyNutrient['fat'] / $this->weeklyGoal['fat'] * 100) : 0,
+            $this->weeklyGoal['carbohydrate'] !== 0 ? round($weeklyNutrient['carbohydrate'] / $this->weeklyGoal['carbohydrate'] * 100) : 0
+        ]) : '';
     }
 
     public function render()
